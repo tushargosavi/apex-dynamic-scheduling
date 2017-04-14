86cad4b79f312456753cbe878988663a5357e8e1
Tushar R. Gosavi <tushar@apache.org>
2017-04-15 00:49:10 +0530

    Remove not required code

--- src/main/java/com/datatorrent/wordcount/lindag/DagSchedulingApp.java
+++ src/main/java/com/datatorrent/wordcount/lindag/DagSchedulingApp.java
@@ -23,64 +23,13 @@ import static org.slf4j.LoggerFactory.getLogger;
 @ApplicationAnnotation(name = "DagSchedulingApp")
 public class DagSchedulingApp implements StreamingApplication
 {
-
-  public static class MyScheduler extends LinearDAGScheduler
-  {
-
-    private Map<String, String> properties = new HashMap<>();
-    private int index = 0;
-
-    public MyScheduler(Configuration conf)
-    {
-      for (Map.Entry<String, String> entry : conf) {
-        properties.put(entry.getKey(), entry.getValue());
-      }
-    }
-
-    private static final Logger LOG = getLogger(MyScheduler.class);
-
-    @Override
-    public boolean populateNextDAG(DAG dag)
-    {
-      LOG.info("Starting dag with index {}", index);
-      StreamingApplication app = null;
-      switch (index) {
-        case 0:
-          app = new AppStage1();
-          break;
-        case 1:
-          app = new AppStage2();
-          break;
-        case 2:
-          app = new AppStage3();
-          break;
-        case 3:
-          return true;
-        default:
-          // should not reach here.
-          System.exit(0);
-      }
-
-      if (app != null) {
-        Configuration conf = new Configuration();
-        for (Map.Entry<String, String> entry : properties.entrySet()) {
-          conf.set(entry.getKey(), entry.getValue());
-        }
-        app.populateDAG(dag, conf);
-        index++;
-        return false;
-      }
-
-      return true;
-    }
-  }
-
   @Override
   public void populateDAG(DAG dag, Configuration conf)
   {
     dag.setAttribute(Context.DAGContext.DEBUG, true);
     SchedulerOperator scheduler = dag.addOperator("Scheduler", new SchedulerOperator());
     dag.getMeta(scheduler).getAttributes().put(OperatorContext.STATS_LISTENERS,
-        Collections.<StatsListener>singletonList(new AppBasedLinearDAGScheduler(conf, AppStage1.class, AppStage2.class, AppStage3.class)));
+        Collections.<StatsListener>singletonList(new AppBasedLinearDAGScheduler(
+          conf, AppStage1.class, AppStage2.class, AppStage3.class)));
   }
 }
